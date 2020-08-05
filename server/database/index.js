e684bdebe4873b5bc5966788091e3dee9d0075b4
Donghao Wu <donghao.wu.us@gmail.com>
2020-08-05 01:26:10 -0400

    ready for deploy

--- server/database/index.js
+++ server/database/index.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg');
 
 const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/weather-db';
-const SSL = process.env.NODE_ENV === 'production';
 
 class Database {
   constructor() {
     this._pool = new Pool({
       connectionString: CONNECTION_STRING,
-      ssl: SSL
     });
 
     this._pool.on('error', (err, client) => {
